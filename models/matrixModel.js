fa472aaacc2f890e182831ca4eec2f7016d5b3bf
andrus92 <yahoraual92@gmail.com>
2022-10-09 11:18:05 +0200

    Created the functions for handling all shifting operations with the matrix

--- models/matrixModel.js
+++ models/matrixModel.js
@@ -30,37 +30,31 @@ MatrixModel.prototype.startNewGame = function() {
     ];
 
     // Generate the first number
-    let coordinates = generateCoordinates(this.attributes.grid);
-    let num = generateNumber();
-    this.attributes.grid[coordinates.x][coordinates.y] = num;
+    generateNewNumber(this.attributes.grid);
 
     // Generate the second number
-    coordinates = generateCoordinates(this.attributes.grid);
-    num = generateNumber();
-    this.attributes.grid[coordinates.x][coordinates.y] = num;
+    generateNewNumber(this.attributes.grid);
 
     // publish the changes
     this.publish('changeData');
 }
 
 MatrixModel.prototype.playGame = function(key) {
-    // console.log(key)
+
     switch(key) {
         case 'ArrowUp':
-            this.attributes.grid[1][1] = 2;
-            break;
         case 'ArrowDown':
-            this.attributes.grid[2][2] = 2;
-            break;
         case 'ArrowRight':
-            this.attributes.grid[3][3] = 2;
-            break;
         case 'ArrowLeft':
-            this.attributes.grid[3][2] = 2;
+            handleArrowPush(this.attributes.grid, key);
             break;
         default:
             return;
     }
-    
-    this.publish('changeData');
+
+    if (generateNewNumber(this.attributes.grid) === true) {
+        this.publish('changeData');
+    } else if (isGameOver(this.attributes.grid) === true) {
+        alert('The end of the game');
+    }
 }
--- utils/logic.js
+++ utils/logic.js
@@ -16,10 +16,256 @@ function generateCoordinates(grid) {
 
     });
 
-    if (possibleCoordinates.length !== 0) {
-        const random = Math.floor(Math.random() * possibleCoordinates.length);
+    const len = possibleCoordinates.length;
+
+    if (len !== 0) {
+        const random = Math.floor(Math.random() * len);
         return {x: possibleCoordinates[random].x, y: possibleCoordinates[random].y};
     }
 
     return undefined
+}
+
+function shiftDown(grid) {
+    const len = grid.length;
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (j = 0; j < len; j += 1) {
+        const col = [];
+        
+        // Getting a j-th column
+        for (i = 0; i < len; i += 1) {
+            col.push(grid[i][j]);
+        }
+
+        // Getting non-'' values 
+        const colFilter = col.filter(x => x !== '');
+        const colFilterLen = colFilter.length;
+
+        // Changing the column
+        if (colFilterLen < len) {
+            const colNew = Array(len - colFilterLen).fill('');
+            colNew.push(...colFilter);
+
+            for (k = 0; k < len; k += 1) {
+                grid[k][j] = colNew[k];
+            } 
+        }
+    } 
+}
+
+
+function shiftUp(grid) {
+    const len = grid.length;
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (j = 0; j < len; j += 1) {
+        const col = [];
+        
+        // Getting a j-th column
+        for (i = 0; i < len; i += 1) {
+            col.push(grid[i][j]);
+        }
+
+        // Getting non-'' values 
+        const colFilter = col.filter(x => x !== '');
+        const colFilterLen = colFilter.length;
+
+        // Changing the column
+        if (colFilterLen < len) {
+            const colNew = Array(len - colFilterLen).fill('');
+            colNew.unshift(...colFilter);
+
+            for (k = 0; k < len; k += 1) {
+                grid[k][j] = colNew[k];
+            } 
+        }
+    }
+}
+
+function shiftRight(grid) {
+    const len = grid.length;
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (i = 0; i < len; i += 1) {
+
+        // Getting non-'' values 
+        const rowFilter = grid[i].filter(x => x !== '');
+        const rowFilterLen = rowFilter.length;
+
+        // Changing the row
+        if (rowFilterLen < len) {
+            const rowNew = Array(len - rowFilterLen).fill('');
+            rowNew.push(...rowFilter);
+
+            for (k = 0; k < len; k += 1) {
+                grid[i][k] = rowNew[k];
+            } 
+        }
+
+    }
+}
+
+function shiftLeft(grid) {
+    const len = grid.length;
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (i = 0; i < len; i += 1) {
+        // Getting non-'' values 
+        const rowFilter = grid[i].filter(x => x !== '');
+        const rowFilterLen = rowFilter.length;
+
+        // Changing the row
+        if (rowFilterLen < len) {
+            const rowNew = Array(len - rowFilterLen).fill('');
+            rowNew.unshift(...rowFilter);
+
+            for (k = 0; k < len; k += 1) {
+                grid[i][k] = rowNew[k];
+            } 
+        }
+    }
+}
+
+
+function downMerge(grid) {
+    let i = 0;
+    let j = 0;
+    const len = grid.length;
+
+    // Comparison of the neighboring values from the bottom in each column and merging them if needed
+    for (j = 0; j < len; j += 1) {
+        for (i = len - 1; i > 0; i -= 1) {
+            if (grid[i][j] !== '') {
+                if (grid[i][j] === grid[i - 1][j]) {
+                    grid[i][j] *= 2;
+                    grid[i - 1][j] = '';
+                }
+            }
+        }
+    }
+}
+
+function upMerge(grid) {
+    let i = 0;
+    let j = 0;
+    const len = grid.length;
+
+    // Comparison of the neighboring values from the top in each column and merging them if needed
+    for (j = 0; j < len; j += 1) {
+        for (i = 0; i < len - 1; i += 1) {
+            if (grid[i][j] !== '') {
+                if (grid[i][j] === grid[i + 1][j]) {
+                    grid[i][j] *= 2;
+                    grid[i + 1][j] = '';
+                }
+            }
+        }
+    }
+}
+
+function rightMerge(grid) {
+    let i = 0;
+    let j = 0;
+    const len = grid.length;
+
+    //Comparison of the neighboring values from the right to the left in each row and merging them if needed
+    for (i = 0; i < len; i += 1) {
+        for (j = len - 1; j > 0; j -= 1) {
+            if (grid[i][j] !== '') {
+                if (grid[i][j] === grid[i][j - 1]) {
+                    grid[i][j] *= 2;
+                    grid[i][j - 1] = '';
+                }
+            }
+        }
+    }
+}
+
+function leftMerge(grid) {
+    let i = 0;
+    let j = 0;
+    const len = grid.length;
+
+    // Comparison of the neighboring values from the left to the right in each row and merging them if needed
+    for (i = 0; i < len; i += 1) {
+        for (j = 0; j < len - 1; j += 1) {
+            if (grid[i][j] !== '') {
+                if (grid[i][j] === grid[i][j + 1]) {
+                    grid[i][j] *= 2;
+                    grid[i][j + 1] = '';
+                }
+            }
+        }
+    }
+}
+
+function handleArrowPush(grid, keyCode) {
+    switch(keyCode) {
+        case 'ArrowUp':
+            shiftUp(grid);
+            upMerge(grid);
+            shiftUp(grid);
+            break;
+        case 'ArrowDown':
+            shiftDown(grid);
+            downMerge(grid);
+            shiftDown(grid);
+            break;
+        case 'ArrowRight':
+            shiftRight(grid);
+            rightMerge(grid);
+            shiftRight(grid);
+            break;
+        case 'ArrowLeft':
+            shiftLeft(grid);
+            leftMerge(grid);
+            shiftLeft(grid);
+            break;
+        default:
+            return;
+    }
+}
+
+function isGameOver(grid) {
+    const flatGridFilter = grid.flat().filter(x => x === '');
+
+    if (flatGridFilter.length > 0) {
+        return false;
+    }
+
+    const keyCodes = ['ArrowUp', 'ArrowDown', 'ArrowRight', 'ArrowLeft'];
+    let gridCopy = [];
+    for (i = 0; i < grid.length; i += 1) {
+        gridCopy[i] = [...grid[i]];
+    }
+
+    for (i = 0; i < keyCodes.length; i += 1) {
+        handleArrowPush(gridCopy, keyCodes[i]);
+        if (gridCopy.toString() !== grid.toString()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function generateNewNumber(grid) {
+    let coordinates = generateCoordinates(grid);
+    
+    if (coordinates === undefined) { 
+        return false;
+    }
+
+    let num = generateNumber();
+    grid[coordinates.x][coordinates.y] = num;
+    return true;
 }
